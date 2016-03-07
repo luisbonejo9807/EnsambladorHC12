4cb426433fe5c556167823dbe207c674c9bec051
moschino19 <b593709@trbvn.com>
2016-03-07 15:22:50 -0600

    Added files via upload

--- src/ensambladorhc12/EnsambladorHC12Raw.java
+++ src/ensambladorhc12/EnsambladorHC12Raw.java
@@ -12,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,7 +22,7 @@ import java.util.logging.Logger;
  */
 public final class EnsambladorHC12Raw {
 
-    final static String FILE_NAME ="src\\ensambladorhc12\\P1ASM.TXT";
+    final static String FILE_NAME ="C:\\Users\\hp\\Documents\\NetBeansProjects\\EnsambladorHC12\\src\\ensambladorhc12\\P1ASM2.TXT";
     private static String contenidoDeArchivo;
     private static ArrayList<String>  comentarios;
     private static ArrayList<String>  codops;
@@ -49,30 +50,40 @@ public final class EnsambladorHC12Raw {
           String contenido = ensamblador.readFile(FILE_NAME,StandardCharsets.UTF_8);
            for (String linea : ensamblador.separarEnLineas(contenido))
            {
-               if(ensamblador.isComentario(linea))
+//               validacion para linea vacia
+//               if(linea.length())
+//               {
+//                   System.out.println("ERROR linea vacia");
+//               }
+               String[] palabra = ensamblador.separarEnPalabras(linea);
+              
+               if(linea.charAt(linea.length()-1) == palabra[palabra.length-1].charAt(palabra[palabra.length-1].length()-1))
+                        System.out.println("END");
+               else if(ensamblador.isComentario(linea))
                {
                    ensamblador.getComentarios().add(linea);
                    System.out.println("COMENTARIO="+ensamblador.getComentarios().get(ensamblador.getComentarios().size()-1));
                }
-               else if((""+linea.charAt(0)).matches("^[a-zA-Z]")&& ensamblador.isETIQUETA(ensamblador.separarEnPalabras(linea)[0]))
+               else 
                {
-                   ensamblador.getEtiquetas().add(ensamblador.separarEnPalabras(linea)[0]);
-                   ensamblador.analizarLinea(ensamblador.separarEnPalabras(linea.substring(ensamblador.separarEnPalabras(linea)[0].length(),linea.length())));
-                   
                    System.out.println("");
-                   System.out.println("ETIQUETA="+ensamblador.getEtiquetas().get(ensamblador.getEtiquetas().size()-1));
-                   System.out.println("CODOP="+ensamblador.getCodops().get(ensamblador.getCodops().size()-1));
-                   System.out.println("OPERANDO="+ensamblador.getOperandos().get(ensamblador.getOperandos().size()-1));
-               }
-               else
-               {
-                   ensamblador.analizarLinea(ensamblador.separarEnPalabras(linea));
-                   ensamblador.getEtiquetas().add("null");
-                   
-                   System.out.println("");
-                    System.out.println("ETIQUETA="+ensamblador.getEtiquetas().get(ensamblador.getEtiquetas().size()-1));
-                   System.out.println("CODOP="+ensamblador.getCodops().get(ensamblador.getCodops().size()-1));
-                   System.out.println("OPERANDO="+ensamblador.getOperandos().get(ensamblador.getOperandos().size()-1));
+                   if(ensamblador.hasETIQUETA(linea))
+                   {
+                       System.out.print(ensamblador.validarETIQUETA(palabra[0]));
+                       if(palabra.length>1)
+                           ensamblador.analizarLinea(Arrays.copyOfRange(palabra, 1, palabra.length));
+                       else
+                       {
+                           System.out.println("CODOP = null");
+                           System.out.println("\tERROR: Si existe una etiqueta debe existir otro token más");
+                           System.out.println("OPERANDO = null\n");
+                       }
+                   }
+                   else
+                   {
+                       System.out.println("ETIQUETA = null");
+                       ensamblador.analizarLinea(palabra);
+                   }
                }
            }
         } catch (IOException ex) {Logger.getLogger(EnsambladorHC12Raw.class.getName()).log(Level.SEVERE, null, ex);}
@@ -92,25 +103,61 @@ public final class EnsambladorHC12Raw {
     }
     
     private void analizarLinea(String[] palabras) {
+        
         this.getCodops().add("null");
         this.getOperandos().add("null");
         
-        this.getCodops().add(palabras[0]);
+        System.out.print(this.validarCODOP(palabras[0]));
         if(palabras.length>1)
         {
             StringBuilder s = new StringBuilder("");
             for (int i = 1; i < palabras.length; i++) 
                s.append(palabras[i]).append(" ");
-            this.getOperandos().add(s.toString());
+            System.out.println(this.validarOPERANDO(s.toString()));
         }
+        else
+             System.out.println("OPERANDO = null\n");
     }
 
     public boolean  isComentario(String linea) {
          return this.separarEnPalabras(linea)[0].startsWith(";");
     }
     
-     private boolean isETIQUETA(String palabra ) {
-        return  palabra.matches("^[a-zA-Z0-9_]{0,7}$");
+    private boolean hasETIQUETA(String linea ) {
+        return !(""+linea.charAt(0)).matches("\\s++");
+    }
+    
+    public String validarOPERANDO(String palabra ) {
+        return  "OPERANDO = "+palabra+"\n";
+    }
+    
+    public String validarCODOP(String palabra ) {
+        StringBuilder s = new StringBuilder("CODOP = "+palabra+"\n");
+        if(palabra.length()>5)
+            return s.append("\tERROR: Tamaño de CODOP mayor a 5\n").toString();
+        else if(!(""+palabra.charAt(0)).matches("^[a-zA-Z]$"))
+                return s.append("\tERROR: Los CODOPS no pueden empezar con carácteres que no sean alfabéticos\n").toString();
+        else if(!palabra.matches("^[^.]*.[^.]*$"))
+            return  s.append("\tERROR: No se puede usar más de 2 veces el caracter \".\" en los CODOPS\n").toString();
+        else if(palabra.length()> 1 &&
+                !(palabra.substring(1).matches("^[a-zA-Z\\.]+$"))
+               ) 
+            return s.append("\tERROR: Existe algún carácter inválido en el CODOP\n").toString();
+        return  s.toString();
+    }
+    
+    private String validarETIQUETA(String palabra ) {
+        StringBuilder s = new StringBuilder("ETIQUETA = "+palabra+"\n");
+        if(palabra.length()>8)
+            return s.append("\tERROR: Tamaño de ETIQUETA mayor a 8\n").toString();
+        else if(!(""+palabra.charAt(0)).matches("^[a-zA-Z]$"))
+            return s.append("\tERROR: Las ETIQUETAS no pueden empezar con carácteres que no sean alfabéticos\n").toString();
+        else if(palabra.length()> 1 &&
+                !(palabra.substring(1).matches("^[a-zA-Z0-9_]{0,7}$"))
+               )
+             return s.append("\tERROR: Existe algún carácter inválido en la ETIQUETA\n").toString();
+        else
+            return s.toString();
     }
 
     public ArrayList<String> getComentarios() {
